ef79e24cab2868d23a909392ffe16a86e76171e4
Nikhil Sarwate <nikhil.sarwate@gmail.com>
2012-10-09 13:47:02 -0700

    complete EPL player list

--- app/models/player.rb
+++ app/models/player.rb
@@ -19,15 +19,29 @@ class Player < ActiveRecord::Base
   belongs_to :position
   belongs_to :fantasy_team
   
-  def self.players_view(team_id='%', position_id='%')
-    if team_id == "" or team_id.nil?
-        team_id = '%'
+  # def self.players_view(team_id='%', position_id='%')
+    # if team_id == "" or team_id.nil?
+        # team_id = '%'
+    # end
+    # if position_id == "" or position_id.nil?
+      # position_id = '%'
+    # end
+    # joins(:team, :position).where("players.team_id = ? and players.position_id = ?", team_id, position_id).includes(:team, :position)
+  # end
+  
+  
+  def self.players_view(team_id=nil, position_id=nil)
+    if team_id.nil? && position_id.nil?
+      joins(:team, :position).all
+    elsif team_id.nil? || team_id == ""
+      joins(:team, :position).where("players.position_id = ?", position_id).includes(:team, :position)
+    elsif position_id.nil? || position_id == ""
+      joins(:team, :position).where("players.team_id = ?", team_id).includes(:team, :position)
+    else
+      joins(:team, :position).where("players.team_id = ? and players.position_id = ?", team_id, position_id).includes(:team, :position)
     end
-    if position_id == "" or position_id.nil?
-      position_id = '%'
-    end
-    joins(:team, :position).where("players.team_id LIKE ? and players.position_id LIKE ?", team_id, position_id).includes(:team, :position)
   end
+  
   
   def self.find_by_name_and_team(first_name, last_name, team)
     team = Team.find_by_name(team)
